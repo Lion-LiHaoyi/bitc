091acbbfb69c1aa2143ae8e05256b0aaad82a513
Lion-LiHaoyi <1476563317@qq.com>
2021-05-21 21:28:40 +0800

    重构格式，增加币价模拟与购买算法

--- bitc.py
+++ bitc.py
@@ -1,16 +1,19 @@
 import random
 file = open('bitc.csv', 'w')
-ff = open('e0e1.txt', 'r')
-ff1 = open('e1e2.txt', 'r')
-count = int(ff.read())
-count1 = int(ff1.read())
+ff = open('count.txt', 'r')
+count1 = ff.readlines()
 ff.close()
-ff1.close()
-file.write('t,n,d,e0,e1,e2,c0,c1,c2\n')
-a, xx, minx = 1, 1, 1
-e1, c1, n1 = -1, 1, 1
+count=[0,0,0,0,0,0]
+for i in range(len(count1)):
+    count[i]=int(count1[i])
+file.write('t,n,e0,e1,e2,e3\n')
+# 数据的预读取和定义
+a, xx = 1, 1
 e0, c0 = -1, 1
-e2, c2, n2, minx1 = -1, 1, 1, 1  # 数据的预读取和定义
+e1, c1, n1, minn1 = -1, 1, 1, 1
+e2, c2, n2, minn2 = -1, 1, 1, 1
+e3, c3, n3, minn3 = -1, 1, 1, 1
+
 for i in range(600):
 
     if c0 != 0:  # 算法0；一跌就卖，一涨就买
@@ -26,63 +29,114 @@ for i in range(600):
         if a >= 1.5 * n1:
             e1 += a * (c1 - c1 * n1 / a)
             c1 *= n1 / a
-            n1 = minx = a
+            n1 = minn1 = a
         elif a <= 0.5 * n1:
             e1 += a * c1
             c1 = 0
-            n1 = minx = a
+            n1 = minn1 = a
     else:
-        if a >= n1 * 2 or (a <= n1 * 0.75 and a > minx):
+        if a >= n1 * 1.8 or (a <= n1 * 0.75 and a > minn1):
             e1 -= 1
             c1 = 1 / a
-            n1 = minx = a
+            n1 = minn1 = a
 
     if c2 != 0:  # 算法2：每上涨2倍则卖至购买价，跌至一半全卖，涨回来或又跌到0.5则买回
         if a >= 2 * n2:
             e2 += a * (c2 - c2 * n2 / a)
             c2 *= n2 / a
-            n2 = minx1 = a
+            n2 = minn2 = a
         elif a <= 0.5 * n2:
             e2 += a * c2
             c2 = 0
-            n2 = minx1 = a
+            n2 = minn2 = a
     else:
-        if a >= n2 * 2 or (a <= n2 * 0.5 and a > minx1):
+        if a >= n2 * 1.8 or (a <= n2 * 0.5 and a > minn2):
             e2 -= 1
             c2 = 1 / a
-            n2 = minx1 = a
+            n2 = minn2 = a
 
-    if e1 > e0:
-        count += 1
+    if c3 != 0:  # 算法2：每上涨2倍则卖至购买价，跌至一半全卖，涨回来或又跌到0.5则买回
+        if a >= 1.2 * n3:
+            e3 += a * (c3 - c3 * n3 / a)
+            c3 *= n3 / a
+            n3 = minn3 = a
+        elif a <= 0.8 * n3:
+            e3 += a * c3
+            c3 = 0
+            n3 = minn3 = a
     else:
-        count -= 1
+        if a >= n3 * 1.2 or (a <= n3 * 0.8 and a > minn2):
+            e3 -= 1
+            c3 = 1 / a
+            n3 = minn3 = a
 
+    if e0 > e1:
+        count[0] += 1
+    elif e1 > e0:
+        count[0] -= 1
+    if e0 > e2:
+        count[1] += 1
+    elif e2 > e0:
+        count[1] -= 1
+    if e0 > e3:
+        count[2] += 1
+    elif e3 > e0:
+        count[2] -= 1
     if e1 > e2:
-        count1 += 1
-    else:
-        count1 -= 1
+        count[3] += 1
+    elif e2 > e1:
+        count[3] -= 1
+    if e1 > e3:
+        count[4] += 1
+    elif e3 > e1:
+        count[4] -= 1
+    if e2 > e3:
+        count[5] += 1
+    elif e3 > e2:
+        count[5] -= 1
 # 生成币价
 
-    x = a * random.randint(80, 125) / 100
-    file.write('{},{},{},{},{},{},{},{},{}\n'.format(i, a, x - a, e0, e1, e2, c0, c1, c2))
+    '''x = a * random.randint(80, 125) / 100'''  # 波动大增长多
+    x = a * random.randint(910, 1099) / 1000  # 波动小增长少
+    file.write('{},{},{},{},{},{}\n'.format(i, a, e0, e1, e2, e3))
     xx = a
     a = x
-    if a < minx:
-        minx = a
-e1 += c1 * a
+    if a < minn1:
+        minn1 = a
+    if a < minn2:
+        minn2 = a
+    if a < minn3:
+        minn3 = a
 e0 += c0 * a
+e1 += c1 * a
 e2 += c2 * a
-file.write('{},{},{},{},{},{},{},{},{}\n'.format(600, a, 0, e0, e1, e2, 0, 0, 0))
-if e1 > e0:
-    count += 10
-else:
-    count -= 10
+e3 += c3 * a
+file.write('{},{},{},{},{},{}\n'.format(600, a, e0, e1, e2, e3))
+if e0 > e1:
+    count[0] += 10
+elif e1 > e0:
+    count[0] -= 10
+if e0 > e2:
+    count[1] += 10
+elif e2 > e0:
+    count[1] -= 10
+if e0 > e3:
+    count[2] += 10
+elif e3 > e0:
+    count[2] -= 10
 if e1 > e2:
-    count1 += 10
-else:
-    count1 -= 10
+    count[3] += 10
+elif e2 > e1:
+    count[3] -= 10
+if e1 > e3:
+    count[4] += 10
+elif e3 > e1:
+    count[4] -= 10
+if e2 > e3:
+    count[5] += 10
+elif e3 > e2:
+    count[5] -= 10
 file.close()
-f = open('e0e1.txt', 'w')
-f.write('{}'.format(count))
-f1 = open('e1e2.txt', 'w')
-f1.write('{}'.format(count1))
+f = open('count.txt', 'w')
+for i in count:
+    f.write('{}\n'.format(i))
